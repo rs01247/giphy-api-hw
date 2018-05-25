19e8078aaa34d5f9a2dd7531858a9328d89c6b52
Raymond <rs01247@gmail.com>
2018-05-24 18:49:23 -0700

    clicks on dynamic elements working, gifs start and pause

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var topics = [
+    let topics = [
         "Kanye West",
         "Travis Scott",
         "Rick Ross",
@@ -9,49 +9,72 @@ $(document).ready(function () {
         "Post Malone"
     ];
 
-    var artistClicked;
-    var gifButton;
-
     function createButtons() {
         $("gif-button").empty();
         topics.forEach(function (event) {
             console.log(event)
-            gifButton = $("<button>");
-            gifButton.attr("class", "topic-button").attr("data-person", event).text(event)
+            const gifButton = $("<button>");
+            gifButton.attr({
+                class: "topic-button btn-dark",
+                "data-person": event
+            });
+            gifButton.text(event)
             $("#gif-button").append(gifButton);
         })
 
     }
+
     // FUNCTION TO GENERATE INITIAL BUTTONS 
     createButtons();
-    var giphyAPI = "rkMz99czA9apZlqQY8uUZr7Amja7v51P";
+    const giphyAPI = "rkMz99czA9apZlqQY8uUZr7Amja7v51P";
 
     $(".topic-button").on("click", function () {
-        console.log(this);
         event.preventDefault()
 
-        artistClicked = $(this).attr("data-person")
-        var queryURL = `https://api.giphy.com/v1/gifs/search?q=${artistClicked}&api_key=${giphyAPI}&limit=10`;
+        let artistClicked = $(this).attr("data-person")
+        const queryURL = `https://api.giphy.com/v1/gifs/search?q=${artistClicked}&api_key=${giphyAPI}&limit=10`;
         console.log(queryURL);
 
         axios.get(queryURL)
             .then(function (resp) {
-                var results = resp.data.data;
+                const results = resp.data.data;
                 console.log(results);
                 results.forEach(function (display) {
                     console.log(display)
-                    var gifImage = $("<img>")
-                    gifImage.attr("class", "artist-gif").attr("src", display.images.fixed_height_small.url)
-                    $("#gif-dump").prepend(gifImage);
-                    // $("#gif-dump").prepend()
+                    const gifImage = $("<img>");
+                    const belowImage = $("<h6 class='ml-2'>");
+                    const stillGif = display.images.fixed_height_still.url;
+                    const animateGif = display.images.fixed_height.url;
+                    const gifRating = display.rating;
+                    gifImage.attr({
+                        class: "artist-gif",
+                        src: stillGif,
+                        "data-still": stillGif,
+                        "data-animate": animateGif,
+                        "data-state": "still"
+                    });
+                    belowImage.text(`Rating: ${gifRating.toUpperCase()}`);
+                    $("#gif-dump").prepend(belowImage).prepend(gifImage);
                 })
-
             })
-
             .catch(function (err) {
                 // console.error(err);
             })
+    })
 
+    $(document).on("click", ".artist-gif", function () {
+
+        const state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate")
+        }
+
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");  
+        }
     })
 
 })
